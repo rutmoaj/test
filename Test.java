845f60ecbd594363a4a1785ffa84f6e94ce5f3cb
rutmoaj <rutmoaj6gmail.com>
2025-12-05 22:21:33 +0100

    using ngrok

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
 	public static void main(String[] args) {
 		System.out.println("Hej Jenkins");
 		System.out.println("Hej test test");
+		System.out.println("Hej ngrok! Det funkar!!");
 	}
 }
